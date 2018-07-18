5b22fa97ba1a635f9dd2d8b169f965e9c9609393
Son Nguyen <snguyenhong@pentalog.fr>
2018-07-18 11:21:48 +0700

    First commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
+
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Invalid email or password'], 401);
+        }
+
+        return response([
+            'status' => 'success'
+        ])
+        ->header('Authorization', $token);
+    }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function user()
+    {
+        return response([
+            'status' => 'success',
+            'data' => auth()->user()
+        ]);
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return response([
+            'status' => 'success'
+        ])->header('Authorization', auth()->refresh());
+        ;
+    }
+}
---
+++ readme.md
@@ -0,0 +1,37 @@
+# Laravel 5.6 + CoreUI Admin Bootstrap + VueJS + JWT
+
+
+## What is this?
+
+This a boilerplate for a SPA Admin application with Laravel 5.6, VueJS, CoreUI and JWT authentication (hope it can save up some time for someone who's developing a project like that).
+It's using following open sources:
+- [laravel-coreui-vue](https://github.com/Braunson/laravel-coreui-vue)
+- [tymon/jwt-auth](https://github.com/tymondesigns/jwt-auth)
+- [websanova/vue-auth](https://github.com/websanova/vue-auth)
+
+
+## What have been implemented:
+
+- integrated Laravel 5.6 with [tymon/jwt-auth](https://github.com/tymondesigns/jwt-auth). There are some small problems when integrate with this library, so I have used the branch 'develop'.
+- integrated [websanova/vue-auth](https://github.com/websanova/vue-auth) with the current [laravel-coreui-vue](https://github.com/Braunson/laravel-coreui-vue), now it can authenticate users from Laravel backend, and authorize on each Vue routes.
+- upgrade BootstrapVue to the latest version, fixed some issues during migration.
+
+
+## Getting Started
+
+```
+composer install
+cp .env.example .env
+php artisan key:generate
+php artisan jwt:secret
+php artisan migrate:refresh --seed
+npm install
+npm run dev
+php artisan serve
+
+```
+
+
+## License
+
+The MIT License (MIT)
